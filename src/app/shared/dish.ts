745be33fdd38030a287de185e81c7f6cd9ac5fea
Ritish Kannayagari <ritish222@gmail.com>
2020-04-22 14:15:01 +0530

    components part 1

---
+++ src/app/shared/dish.ts
@@ -0,0 +1,11 @@
+export class Dish{
+    id: string;
+    name: string; 
+    image: string;
+    category: string;
+    featured: boolean;
+    price: string;
+    label: string;
+    description: string;
+
+}
