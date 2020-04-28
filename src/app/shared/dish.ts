d072af46242ef496575a9943cd3238eee8a2d378
Ritish Kannayagari <ritish222@gmail.com>
2020-04-28 13:03:54 +0530

    Data Binding

--- src/app/shared/dish.ts
+++ src/app/shared/dish.ts
@@ -1,3 +1,4 @@
+import {Comment} from './comment';
 export class Dish{
     id: string;
     name: string; 
@@ -7,5 +8,5 @@ export class Dish{
     price: string;
     label: string;
     description: string;
-
+    comments: Comment[];
 }
