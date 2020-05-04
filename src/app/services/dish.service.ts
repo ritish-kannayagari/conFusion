968e2364202c6a7ad7d20dec507e88a7bb0af924
Ritish Kannayagari <ritish222@gmail.com>
2020-05-04 13:40:51 +0530

    Single Page Application part 1

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {Dish} from '../shared/dish';
 import {DISHES} from '../shared/dishes';
+import { Z_FINISH } from 'zlib';
 
 @Injectable({
   providedIn: 'root'
@@ -11,5 +12,11 @@ export class DishService {
 
   getDishes(): Dish[]{
     return DISHES;
+  }
+  getDish(id:string):Dish{
+    return DISHES.filter((dish)=>(dish.id==id))[0];
+  }
+  getFeaturedDish():Dish{
+    return DISHES.filter((dish)=>(dish.featured))[0];
   }
 }
